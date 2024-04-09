5c587626f9ffc8f7f6960d6cc199134eeb1d79c5
Dev Chauhan <124146292+DevChauhan5@users.noreply.github.com>
2024-04-09 17:06:37 +0000

    init

--- app/(protected)/dashboard/calendar/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Calendar = () => {
-  return (
-    <div>This Feature is Comming Soon !</div>
-  )
-}
-
-export default Calendar
--- app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Loader = () => {
-  return (
-    <div className='min-h-screen flex items-center justify-center text-5xl animate-pulse'> taskit ...</div>
-  )
-}
-
-export default Loader
